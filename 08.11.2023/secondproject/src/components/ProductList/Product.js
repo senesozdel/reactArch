4a31c12848952135ac564bfb401be5b5cb0bc011
Salih Enes ÖZDEL <salihenesozdel@gmail.com>
2023-11-08 15:42:19 +0300

    2 Input UserLogin Component is Made
    By using 2 inputs and a button, infos are taken and showed on scrren. UseState and Bootstrap are used.

--- 08.11.2023/secondproject/src/components/ProductList/Product.js
+++ 08.11.2023/secondproject/src/components/ProductList/Product.js
@@ -1,15 +1,42 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Input, Button } from 'reactstrap';
 
 const Product = () => {
+
+const [username,setUsername] = useState(" ")
+const [password,setPassword] = useState(" ")
+
+const [productDetails,setProductDetails ] = useState([])
+
   return (
-    <div>
+
+<div className='d-flex gap-3'>
+<div className='products w-25 d-flex flex-column gap-2'>
         <h3>
             Product Details
         </h3>
-            
+            <Input placeholder='Username' onChange={(e)=> setUsername(e.target.value)} />
+            <Input placeholder='info@gmail.com' onChange={(e)=>setPassword(e.target.value)} />
+            <Button onClick={()=> setProductDetails([...productDetails,[username,password]])} class="btn btn-info">Listele </Button>
     </div>
+    <ul className='product-card d-flex flex-column gap-2 w-25 p-0'>
+    <li className='list-unstyled  '>
+      <h3>
+      Kullanıcı Bilgileri
+      </h3></li>
+    {productDetails.map((item)=><li className='list-unstyled list-group-item '>{`User Name: ${item[0]}  Password: ${item[1]} `}</li>)}
+
+    
+    </ul>
+</div>
+
+
   )
 }
 
 export default Product
+
+
+
+
+
